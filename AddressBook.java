b8963e39ccaa3ae984f7700bb47814a2d6012089
Srikanthnaid <ksru044gmail.com>
2022-09-24 11:06:31 +0530

    Added new contact to AddressBook

--- AddressBook.java
+++ AddressBook.java
@@ -1,7 +1,55 @@
 package com.AddressBook;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBook {
+	static Scanner input = new Scanner(System.in);
+	static ArrayList<ContactInfo> contactBook = new ArrayList<>();
+
+	public void addContact() {
+		System.out.print("Enter First Name:- ");
+		String first = input.next();
+
+		System.out.print("Enter Last Name:- ");
+		String last = input.next();
+
+		System.out.print("Enter Address:- ");
+		String add = input.next();
+
+		System.out.print("Enter City:- ");
+		String city = input.next();
+
+		System.out.print("Enter State:- ");
+		String state = input.next();
+
+		System.out.print("Enter Zip Code:- ");
+		int zip = input.nextInt();
+
+		System.out.print("Enter Phone Number:- ");
+		long phone = input.nextLong();
+
+		System.out.print("Enter E-mail:- ");
+		String email = input.next();
+
+		ContactInfo contact = new ContactInfo(first, last, add, city, state, zip, phone, email);
+		contactBook.add(contact);
+
+		System.out.println("\nContact added Successfully.");
+	}
+
+	public void displayContacts() {
+		System.out.println("\n---------------Contacts in Address Book----------------");
+		for (ContactInfo person : contactBook) {
+			System.out.println(person.toString());
+		}
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome To Address Book Program.");
+		System.out.println("--------------------Welcome To Address Book Program-----------------------");
+		AddressBook obj = new AddressBook();
+		obj.addContact();
+		obj.displayContacts();
+
 	}
 }
--- ContactInfo.java
+++ ContactInfo.java
@@ -7,10 +7,10 @@ public class ContactInfo {
 	String city;
 	String state;
 	int zip_code;
-	int phone_number;
+	long phone_number;
 	String email;
 
-	public ContactInfo(String fn, String ln, String add, String city, String state, int zip, int ph, String email) {
+	public ContactInfo(String fn, String ln, String add, String city, String state, int zip, long phone, String email) {
 
 		this.first_Name = fn;
 		this.last_Name = ln;
@@ -18,8 +18,72 @@ public class ContactInfo {
 		this.city = city;
 		this.state = state;
 		this.zip_code = zip;
-		this.phone_number = ph;
+		this.phone_number = phone;
 		this.email = email;
+	}
+
+	public String getFirst_Name() {
+		return first_Name;
+	}
+
+	public void setFirst_Name(String first_Name) {
+		first_Name = first_Name;
+	}
+
+	public String getLast_Name() {
+		return last_Name;
+	}
+
+	public void setLast_Name(String last_Name) {
+		last_Name = last_Name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		state = state;
+	}
+
+	public int getZip_code() {
+		return zip_code;
+	}
+
+	public void setZip_code(int zip_code) {
+		zip_code = zip_code;
+	}
+
+	public long getPhone_number() {
+		return phone_number;
+	}
+
+	public void setPhone_number(long phone_number) {
+		phone_number = phone_number;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		email = email;
 	}
 
 	public String toString() {
